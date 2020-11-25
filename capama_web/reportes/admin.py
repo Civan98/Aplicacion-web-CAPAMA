100d1c389130d8834cd9639be80e8ebd5c92bad5
AldoValdezS <52605780+AldoValdezS@users.noreply.github.com>
2020-11-25 00:21:54 -0600

    detalles sitio de admin
    borré y volví a crear la bd, debido a un error con postgres, que no me permitía hacer migrate debido a que: 'el valore esa muy grande' Related Field has invalid lookup: icontains”

--- capama_web/reportes/admin.py
+++ capama_web/reportes/admin.py
@@ -2,9 +2,38 @@ from django.contrib import admin
 from .models import Usuarios, Empleados, ReportesUsuario, ReportesEmpleado, Materiales
 # Register your models here.
 
+# mostrar los datos de los registros
 
-admin.site.register(Usuarios)
-admin.site.register(Empleados)
-admin.site.register(ReportesUsuario)
-admin.site.register(ReportesEmpleado)
-admin.site.register(Materiales)
+class UsuariosAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellidos','email','contrasena', 'num_contrato', 'telefono')
+    # para la barra de búsqueda, se puede usar el núm_contrato, telégono, email
+    search_fields = ('num_contrato','telefono','email')
+
+admin.site.register(Usuarios, UsuariosAdmin)
+
+class EmpleadosAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellidos','email','contrasena', 'telefono','cargo', 'num_empleado')
+    list_filter = ( 'cargo', 'email')
+    search_fields = ('num_empleado','telefono','email')
+
+admin.site.register(Empleados, EmpleadosAdmin)
+
+class ReportesUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id','zona', 'folio_seguimiento','prioridad','fecha', 'cp', 'colonia', 'estado', 'id_usuario')
+    list_filter = ('prioridad', 'fecha', 'estado', 'id_usuario', 'zona', 'tipo_anomalia','tipo_servicio')
+    search_fields = ('folio_seguimiento','colonia')
+admin.site.register(ReportesUsuario, ReportesUsuarioAdmin)
+
+class ReportesEmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('id','zona', 'folio_seguimiento','prioridad','fecha_inicio', 'estado', 'id_empleado')
+    list_filter = ('prioridad', 'estado', 'id_empleado', 'zona','tipo_anomalia','tipo_servicio')
+    search_fields = ('folio_seguimiento','id_empleado__num_empleado')
+
+admin.site.register(ReportesEmpleado, ReportesEmpleadoAdmin)
+
+class MaterialesAdmin(admin.ModelAdmin):
+    list_display = ('id_reporte_empleado','material', 'cantidad')
+    list_filter = ('id_reporte_empleado','material')
+    search_fields = ('id_reporte_empleado__folio_seguimiento','material')
+
+admin.site.register(Materiales, MaterialesAdmin)
--- capama_web/reportes/models.py
+++ capama_web/reportes/models.py
@@ -8,6 +8,17 @@ class Usuarios(models.Model):
     contrasena = models.CharField(max_length = 45)
     num_contrato = models.CharField(max_length = 45)
     telefono = models.CharField(max_length = 10)
+    
+    # para mostrar en el sitio de administración la llave foránea, pero de forma más detallada
+    # también para que al mostrar la tabla, se muestren los registros de forma detallada
+    # el id marca error porque no está definido aquí en el modelo, pero este se asignó de forma automática
+
+    def __str__(self):
+        return ("%d.- %s %s" % ( self.id, self.nombre, self.apellidos))
+    
+    
+    class Meta:
+        verbose_name_plural= 'Usuarios'
 
 class Empleados(models.Model):
     # son los diferentes cargos y las opciones que saldrán en el panel de administración
@@ -24,6 +35,13 @@ class Empleados(models.Model):
     cargo = models.CharField(max_length=20, choices = CARGOS, default= CARGOS)
     num_empleado = models.CharField(max_length = 45)
 
+
+    def __str__(self):
+        return ("%d.- %s %s" % ( self.id, self.nombre, self.apellidos))
+
+    class Meta:
+        verbose_name_plural= 'Empleados'
+
 class ReportesUsuario (models.Model):
     PRIORIDAD = (
         ('Leve', 'Leve'),
@@ -38,47 +56,53 @@ class ReportesUsuario (models.Model):
         ('Atendido', 'Atendido')
     )
 
-    TIPOS = [
-        ('Zona', (
-                ('Nuevos desarrollos','Nuevos desarrollos'),
-                ('Urbana', 'Urbana'),
-                ('Conurbada', 'Conurbada'),
-            )
-        ),
-        ('Anomalia', (
-                 ('Falta de agua','Falta de agua'),
-                 ('Falta de presion', 'Falta de presion'),
-                 ('Fuga en la red Hidraulica', 'Fuga en la red Hidraulica'),
-                 ('Fuga en la toma','Fuga en la toma'),
-                 ('Inspeccion', 'Inspeccion'),
-                 ('Material listo', 'Material listo'),
-                 ('Toma obstruida','Toma obstruida'),
-                 ('Toma desconectada', 'Toma desconectada'),
-                 ('Desazolve', 'Desazolve'),
-            )
-        ),
-        ('Servicio', (
-                ('Agua potable','Agua potable'),
-                ('Pipas', 'Pipas'),
-                ('Alcantarillado', 'Alcantarillado')
-            )
-        ),  
-    ]
+    ZONA = (        
+        ('Nuevos desarrollos','Nuevos desarrollos'),
+        ('Urbana', 'Urbana'),
+        ('Conurbada', 'Conurbada')
+    )
+    TIPOANOMALIA = (
+        ('Falta de agua','Falta de agua'),
+        ('Falta de presion', 'Falta de presion'),
+        ('Fuga en la red Hidraulica', 'Fuga en la red Hidraulica'),
+        ('Fuga en la toma','Fuga en la toma'),
+        ('Inspeccion', 'Inspeccion'),
+        ('Material listo', 'Material listo'),
+        ('Toma obstruida','Toma obstruida'),
+        ('Toma desconectada', 'Toma desconectada'),
+        ('Desazolve', 'Desazolve')
+    )
 
-    tipo = models.CharField(max_length = 40, choices=TIPOS, default= TIPOS)
+    TIPOSERVICIO =(       
+        ('Agua potable','Agua potable'),
+        ('Pipas', 'Pipas'),
+        ('Alcantarillado', 'Alcantarillado')
+    )
+
+    zona = models.CharField(max_length = 50, choices=ZONA, default= ZONA )
+    tipo_anomalia = models.CharField(max_length = 50, choices=TIPOANOMALIA, default= TIPOANOMALIA)
+    tipo_servicio = models.CharField(max_length = 50, choices=TIPOSERVICIO, default= TIPOSERVICIO)
     folio_seguimiento = models.CharField(max_length = 50, blank = True)
     foto = models.CharField(max_length = 50)
     prioridad = models.CharField(max_length = 20, choices=PRIORIDAD, default= PRIORIDAD)
-    geoLocalizacion = models.CharField(max_length = 100)
+    geoLocalizacion = models.CharField(max_length = 100, verbose_name='Geolocalización')
     colonia = models.CharField(max_length = 50)
     calle = models.CharField(max_length = 50)
-    cp = models.IntegerField(blank = True)
+    cp = models.IntegerField(blank = True, verbose_name='código postal')
     num_interior = models.IntegerField(blank = True)
     num_exterior = models.IntegerField(blank = True)
     descripcion = models.CharField(max_length = 200)
     fecha = models.DateTimeField(auto_now_add = True)
     estado = models.CharField(max_length = 20, choices = ESTADOS, default=ESTADOS)
     id_usuario = models.ForeignKey(Usuarios, on_delete= models.CASCADE)
+
+    # mostrar el folio de seguimiento
+    def __str__(self):
+        return self.folio_seguimiento
+    
+
+    class Meta:
+        verbose_name_plural= 'Reportes del usuario'
 
 
 class ReportesEmpleado (models.Model):
@@ -87,44 +111,40 @@ class ReportesEmpleado (models.Model):
         ('Moderada', 'Moderada'),
         ('Grave', 'Grave')
     )
-
     ESTADOS = (
         ('Nuevo','Nuevo'),
         ('En proceso', 'En proceso'),
         ('Monitoreado', 'Monitoreado'),
         ('Atendido', 'Atendido')
     )
+    ZONA = (        
+        ('Nuevos desarrollos','Nuevos desarrollos'),
+        ('Urbana', 'Urbana'),
+        ('Conurbada', 'Conurbada')
+    )
+    TIPOANOMALIA = (
+        ('Falta de agua','Falta de agua'),
+        ('Falta de presion', 'Falta de presion'),
+        ('Fuga en la red Hidraulica', 'Fuga en la red Hidraulica'),
+        ('Fuga en la toma','Fuga en la toma'),
+        ('Inspeccion', 'Inspeccion'),
+        ('Material listo', 'Material listo'),
+        ('Toma obstruida','Toma obstruida'),
+        ('Toma desconectada', 'Toma desconectada'),
+        ('Desazolve', 'Desazolve')
+    )
 
-    TIPOS = [
-        ('Zona', (
-                ('Nuevos desarrollos','Nuevos desarrollos'),
-                ('Urbana', 'Urbana'),
-                ('Conurbada', 'Conurbada'),
-            )
-        ),
-        ('Anomalia', (
-                 ('Falta de agua','Falta de agua'),
-                 ('Falta de presion', 'Falta de presion'),
-                 ('Fuga en la red Hidraulica', 'Fuga en la red Hidraulica'),
-                 ('Fuga en la toma','Fuga en la toma'),
-                 ('Inspeccion', 'Inspeccion'),
-                 ('Material listo', 'Material listo'),
-                 ('Toma obstruida','Toma obstruida'),
-                 ('Toma desconectada', 'Toma desconectada'),
-                 ('Desazolve', 'Desazolve'),
-            )
-        ),
-        ('Servicio', (
-                ('Agua potable','Agua potable'),
-                ('Pipas', 'Pipas'),
-                ('Alcantarillado', 'Alcantarillado')
-            )
-        ),  
-    ]
+    TIPOSERVICIO =(       
+        ('Agua potable','Agua potable'),
+        ('Pipas', 'Pipas'),
+        ('Alcantarillado', 'Alcantarillado')
+    )
     # la fecha de inicio se cambiará el auto_now_add
     fecha_inicio = models.DateTimeField(auto_now_add = True)
     fecha_fin = models.DateTimeField(auto_now_add = True)
-    tipo = models.CharField(max_length = 40, choices=TIPOS, default= TIPOS)
+    zona = models.CharField(max_length = 40, choices=ZONA, default= ZONA)
+    tipo_anomalia = models.CharField(max_length = 50, choices=TIPOANOMALIA, default= TIPOANOMALIA)
+    tipo_servicio = models.CharField(max_length = 50, choices=TIPOSERVICIO, default= TIPOSERVICIO)
     folio_seguimiento = models.CharField(max_length = 50, blank = True)
     foto = models.CharField(max_length = 50)
     prioridad = models.CharField(max_length = 20, choices=PRIORIDAD, default= PRIORIDAD)
@@ -141,10 +161,20 @@ class ReportesEmpleado (models.Model):
     estado = models.CharField(max_length = 20, choices = ESTADOS, default=ESTADOS)
     id_empleado = models.ForeignKey(Empleados, on_delete = models.CASCADE)
     id_usuario = models.ForeignKey(Usuarios, on_delete = models.DO_NOTHING)
+    
+    # mostrar el folio de seguimiento
+    def __str__(self):
+        return self.folio_seguimiento
+    
+    class Meta:
+        verbose_name_plural= 'Reportes del empleado'
 
 class Materiales (models.Model):
     material = models.CharField(max_length = 50)
     cantidad = models.IntegerField()
     id_reporte_empleado = models.ForeignKey(ReportesEmpleado, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural= 'Materiales'
+    
 
